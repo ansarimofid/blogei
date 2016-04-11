80a996cb5ccec8d974bccdfa68517404d4f521cc
ansarimofid <ansarimofid@gmail.com>
2016-04-12 02:03:04 +0530

    Added tag support

---
+++ content/post-tags.php
@@ -0,0 +1,14 @@
+<?php
+$tags   = get_the_tags( $post->ID );
+$output = '';
+if ( $tags ) {
+	echo "<div class='article-tag'>";
+	 echo "<ul>";
+
+	 		foreach ( $tags as $tag ) {
+				echo '<li><a href="' . esc_url( get_tag_link( $tag->term_id ) ) . '" title="' . esc_attr( sprintf( __( "View all posts tagged %s", 'founder' ), $tag->name ) ) . '">' . $tag->name . '</a></li>';
+			}
+	echo "</ul>";
+	echo "</div>";
+
+}
